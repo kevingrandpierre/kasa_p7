3102620b79bbd980f304745f0448a42cc29c68b5
kevingrandpierre <grandpierre.kevin@gmail.com>
2023-08-15 02:11:15 +0200

    modify css

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "../../assets/images/LOGO.png";
 import "./header.css";
 
 function Header() {
+    const location = useLocation();
+
     return (
         <header>
             <nav className="navigation">
@@ -11,10 +14,12 @@ function Header() {
                 </div>
                 <ul className="nav_links">
                     <li>
-                        <a href="/">Accueil</a>
+                        {/*Permet de définir un lien vers la page d'accueil et de le souligner s'il n'est pas actif*/}
+                        <Link to="/" className={location.pathname === "/" ? "active" : ""}>Accueil</Link>
                     </li>
                     <li>
-                        <a href="/about">A propos</a>
+                        {/* Permet de définir un lien vers la page about et de le souligner s'il n'est pas actif  */}
+                        <Link to="/about" className={location.pathname === "/about" ? "active" : ""}>A propos</Link>
                     </li>
                 </ul>
             </nav>
